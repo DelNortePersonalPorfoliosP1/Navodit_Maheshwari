4f7d289995ffae59cfc4f5cfa5700ec17d6d42fb
Navodit Maheshwari <navoditmaheshwari@gmail.com>
2019-09-20 09:17:59 -0700

    saving the code and commitfng for today

---
+++ NavoditLabTry3/NavoditLabTry3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  NavoditLabTry3
+//
+//  Created by Maheshwari, Navodit on 9/20/19.
+//  Copyright © 2019 Maheshwari, Navodit. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
